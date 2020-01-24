b2165c5b7b42e793b85d7aa3d28a4f5ba1beb416
Leta Keane <letakeane@users.noreply.github.com>
2020-01-24 10:48:26 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 window.onload = () => {
   document.querySelector('body').addEventListener('keydown', (event) => {
     if (event.key === 65) {
+      console.log('A');
       document.querySelector('.option1').click();
     } else if (event.key === 66) {
+      console.log('B');
       document.querySelector('.option2').click();
     }
   })
