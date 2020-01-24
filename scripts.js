12beab132c11a499442613f0d7751e726b2a0154
Leta Keane <letakeane@users.noreply.github.com>
2020-01-24 11:23:41 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
 const option1 = document.querySelector('.option1');
-const option2 = document.querySelector('.option2')
+const option2 = document.querySelector('.option2');
 
-option1.addEventListener('click', clickAnchorTag);
-option2.addEventListener('click', clickAnchorTag);
+window.onload = function() {
+  option1.addEventListener('click', clickAnchorTag);
+  option2.addEventListener('click', clickAnchorTag);
 
-document.querySelector('body').addEventListener('keydown', function(event) {
-  console.log(event.key);
-  if (event.key == 'a') {
-    option1.click();
-  } else if (event.key == 'b') {
-    option2.click();
-  }
-});
+  document.querySelector('body').addEventListener('keydown', function(event) {
+    console.log(event.key);
+    if (event.key == 'a') {
+      option1.click();
+    } else if (event.key == 'b') {
+      option2.click();
+    }
+  });
+}
+
 
 function clickAnchorTag(event) {
   const event = document.createEvent('MouseEvent');
@@ -19,5 +22,3 @@ function clickAnchorTag(event) {
   const clickedLink = event.target;
   clickedLink.dispatchEvent(event);
 };
-
-console.log('connected')
