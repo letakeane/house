deaaa73b30e98e5eba576a8af761fa1b8a1089b3
Leta Keane <letakeane@users.noreply.github.com>
2020-01-24 10:56:33 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,18 @@
 window.onload = () => {
+  document.querySelector('.option1').addEventListener('click', clickAnchorTag);
+  document.querySelector('.option2').addEventListener('click', clickAnchorTag);
   document.querySelector('body').addEventListener('keydown', (event) => {
-    console.log(event.key);
     if (event.key === 65) {
-      console.log('A ');
       document.querySelector('.option1').click();
     } else if (event.key === 66) {
-      console.log('B');
       document.querySelector('.option2').click();
     }
   })
+}
+
+const clickAnchorTag = (event) => {
+  const event = document.createEvent('MouseEvent');
+  event = new CustomEvent('click');
+  const clickedLink = event.target;
+  clickedLink.dispatchEvent(event);
 }
