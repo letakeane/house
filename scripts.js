86c9e9c2ba9f9691808c5d5a67edf28b41ad6fa5
Leta Keane <letakeane@users.noreply.github.com>
2020-01-24 11:12:51 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 const option1 = document.querySelector('.option1');
 const option2 = document.querySelector('.option2')
 
-const clickAnchorTag = (event) => {
+option1.addEventListener('click', clickAnchorTag);
+option2.addEventListener('click', clickAnchorTag);
+
+function clickAnchorTag(event) {
   const event = document.createEvent('MouseEvent');
   event = new CustomEvent('click');
   const clickedLink = event.target;
   clickedLink.dispatchEvent(event);
+  console.log('clickable')
 }
-
-option1.addEventListener('click', clickAnchorTag);
-option2.addEventListener('click', clickAnchorTag);
 
 document.querySelector('body').addEventListener('keydown', (event) => {
   console.log(event.key);
@@ -20,6 +21,3 @@ document.querySelector('body').addEventListener('keydown', (event) => {
     option2.click();
   }
 })
-
-
-console.log('linked')
